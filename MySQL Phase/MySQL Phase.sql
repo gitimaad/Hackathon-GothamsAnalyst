365d700d4939126f944084ce36dfc847a223b14c
Mohammed Imaad <94297375+gitimaad@users.noreply.github.com>
2025-03-02 18:52:28 +0530

    Add files via upload

---
+++ MySQL Phase/MySQL Phase.sql
@@ -0,0 +1,8 @@
+# Creating a database so that we can connect our Python file. 
+CREATE DATABASE aviation_database;
+
+# Using the database.
+USE aviation_database;
+
+# Selecting the database to see if the the dataset has been added. 
+SELECT * FROM aviation_table;
